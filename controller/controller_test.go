ce6113440e084099aa70b5c1616c2901eaed0f2d
Matthew Wong <mawong@redhat.com>
2019-04-11 17:57:21 -0400

    Cherry-pick #40 on release-1.14

--- controller/controller_test.go
+++ controller/controller_test.go
@@ -1201,7 +1201,8 @@ func constructProvisionedVolumeWithoutStorageClassInfo(claim *v1.PersistentVolum
 	// TODO implement options.ProvisionerSelector parsing
 	// pv.Labels MUST be set to match claim.spec.selector. The provisioner MAY add additional labels.
 
-	volume.ObjectMeta.Finalizers = append(volume.ObjectMeta.Finalizers, finalizerPV)
+	// TODO addFinalizer is false by default
+	// volume.ObjectMeta.Finalizers = append(volume.ObjectMeta.Finalizers, finalizerPV)
 
 	return volume
 }
